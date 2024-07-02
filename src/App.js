0804c6e27aefa900aaf078883a8b8dbe66602c9b
ShivaHariniB <harinibalakumar02@gmail.com>
2024-07-02 13:30:05 +0530

    Implementing sorting unit using derived state

--- src/App.js
+++ src/App.js
@@ -77,17 +77,40 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItems, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("packed");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items.slice().sort((a, b) => Number(a.packed) - b.packed);
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((items) => (
+        {sortedItems.map((items) => (
           <Item
             item={items}
             onDeleteItems={onDeleteItems}
             onToggleItem={onToggleItem}
+            key={items.id}
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description </option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
